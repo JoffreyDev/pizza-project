cebe27c43e1fac9ecb3aea354cd6685963ba29a4
Joffrey <muzhyke1@gmail.com>
2021-06-17 00:38:31 +0300

    Cart improvements

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,13 @@ export const addPizzaToCart = (pizzaObj) => ({
   type: 'ADD_PIZZA_TO_CART',
   payload: pizzaObj,
 });
+
+export const delPizzaFromCart = (id) => ({
+  type: 'DEL_PIZZA_FROM_CART',
+  payload: id,
+});
+
+export const delAllPizzasFromCart = () => ({
+  type: 'DEL_ALL_PIZZAS_FROM_CART',
+  payload: '',
+});
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -7,20 +7,54 @@ const initialState = {
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PIZZA_TO_CART':
+      const currentPizzasItems = !state.items[action.payload.id]
+        ? [action.payload]
+        : [...state.items[action.payload.id].items, action.payload];
+
       const newItems = {
         ...state.items,
-        [action.payload.id]: !state.items[action.payload.id]
-          ? [action.payload]
-          : [...state.items[action.payload.id], action.payload],
+        [action.payload.id]: {
+          items: currentPizzasItems,
+          totalPrice: [].concat
+            .apply([], Object.values(currentPizzasItems))
+            .reduce((sum, obj) => obj.price + sum, 0),
+        },
       };
+
+      const items = Object.values(newItems).map((obj) => obj.items);
+      const allPizzas = [].concat.apply([], items);
+      const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0);
 
       return {
         ...state,
         items: newItems,
-        totalCount: [].concat.apply([], Object.values(newItems)).length,
-        totalPrice: [].concat
-          .apply([], Object.values(newItems))
-          .reduce((sum, obj) => obj.price + sum, 0),
+        totalCount: allPizzas.length,
+        totalPrice: totalPrice,
+      };
+
+      break;
+
+    case 'DEL_PIZZA_FROM_CART':
+      delete state.items[action.payload];
+      const itemsDeleted = Object.values(state.items).map((obj) => obj.items);
+      const allPizzasDeleted = [].concat.apply([], itemsDeleted);
+      const totalPriceDeleted = allPizzasDeleted.reduce((sum, obj) => obj.price + sum, 0);
+
+      return {
+        ...state,
+        items: state.items,
+        totalCount: allPizzasDeleted.length,
+        totalPrice: totalPriceDeleted,
+      };
+
+      break;
+
+    case 'DEL_ALL_PIZZAS_FROM_CART':
+      return {
+        ...state,
+        items: {},
+        totalCount: 0,
+        totalPrice: 0,
       };
   }
   return state;
